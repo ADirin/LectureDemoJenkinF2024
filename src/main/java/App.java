0654df2a0a71a09620c027d01375ba8754180f99
amirdi <amir.dirin@yahoo.com>
2024-09-13 21:09:09 +0300

    First Init

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public int addMe(int a, int b){
+        return a+b;
+    }
+    public int subMe(int a, int b){
+        return a-b;
+    }
+
+}
